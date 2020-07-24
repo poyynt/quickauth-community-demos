8e71a73365a468503c9de9e50eb4265945f8c7b1
Parsa Torbati <ptorbatii@gmail.com>
2020-07-25 01:28:19 +0430

    make my code cleaner

--- poyynt/script.py
+++ poyynt/script.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, redirect
-from requests import post, get
-from urllib.parse import quote 
+from flask import Flask, request
+from requests import post, get 
 import json
 
 
@@ -12,22 +11,23 @@ CALLBACK = "https://localhost:2345/callback"
 def ind():
     return f"""
     <html>
+    <head><title>Alles Quickauth Demo</title></head>
+    <body>
     <a href="https://quickauth.alles.cx/?redirect={CALLBACK}">auth!</a>
+    </body>
     </html>
     """
 
 @app.route("/callback")
 def cb():
-    token = request.args.get("token")
+    token = request.args.get("token") # GET `token` parameter
     req = post("https://quickauth.alles.cx", json = {"token": token, "redirect": CALLBACK})
-    res = json.loads(req.text)["id"]
-    addt = res
-    uid = res.strip()
-    addt += uid
+    uid = json.loads(req.text)["id"]
     namereq = get(f"https://horizon.alles.cc/users/{uid}")
     name = json.loads(namereq.text)["name"]
     return f"""
     <html>
+    <head><title>Alles Quickauth Demo</title></head>
     <body>
     <p>Hello, {name}!</p>
     </body>
